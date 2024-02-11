f63f89341e21f23c4dba4dd6a2c2d7f366fab3d1
IrvinTM <irvint698@gmail.com>
2024-02-10 18:51:23 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const data = require("./data.json");
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send('<h1>Usefull api</h1>');
+});
+
+app.get("/websites", (req, res) => {
+    const { category } = req.query;
+  
+    if (category) {
+        const websites = data.resources.find(resource => resource.category.toLowerCase() === category.toLowerCase());
+        if (websites) {
+          res.send(websites.sites);
+        } else {
+          res.status(404).send("Category not found");
+        }
+      } else {
+        res.send(data.resources);
+      }
+}
+);
+
+const port = process.env.PORT ?? 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
