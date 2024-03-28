751c4fe47959e3e9c968c5b17e2f29fbc3bada8d
IrvinTM <irvint698@gmail.com>
2024-03-28 14:31:29 -0600

    added default value for id param

--- models/postgres.js
+++ models/postgres.js
@@ -88,7 +88,7 @@ export class websiteModel {
         WHERE LOWER(name) = LOWER($1)
         LIMIT 1;`;
       const { rows } = await pool.query(categoryQuery, [newWebsite.category]);
-      const existentCategory = rows[0].id;
+      const existentCategory = rows[0].id || "";
       if (rows[0].id.length === 0) {
         try {
           const query = `INSERT INTO categories (id, name) VALUES ($1, $2) RETURNING *;`;
